982d343a60c9196dd63d86fab35fd7c5f76f4ebb
Senna Van Londersele <senna.vanlondersele@student.odisee.be>
2017-12-29 03:53:58 +0100

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://senna-cablechat.herokuapp.com/cable'
+  config.action_cable.url = 'wss://sitepoint-actioncable.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://senna-cablechat.herokuapp.com', 'http://senna-cablechat.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
